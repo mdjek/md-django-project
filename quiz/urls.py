00c99f49af42c4e9a223ffd730bbb84580e5cfc9
Polina Bondar <bondarpolya@yandex.ru>
2025-01-22 11:05:06 +0300

    the test start page was added

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
-from .views import TestView, AccessTestView, SignUpView, UserTestResultsView
+from .views import TestView, AccessTestView, SignUpView, UserTestResultsView, TestDescriptionView
 
 urlpatterns = [
     path('', AccessTestView.as_view(), name='home'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('log-in', LoginView.as_view(), name='log_in'),
     path('log-out', LogoutView.as_view(), name='log_out'),
     path('access-test', AccessTestView.as_view(), name='access_test'),
-    path('tests/<int:test_id>', TestView.as_view(), name='take_test'),
+    path('test/<int:test_id>/description/', TestDescriptionView.as_view(), name='test_description'), 
+    path('test/<int:test_id>/', TestView.as_view(), name='take_test'),
     path('results', UserTestResultsView.as_view(), name='user_results')
 ]
