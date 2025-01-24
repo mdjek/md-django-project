457c5557c5922d7a2be4e45eb0b89541a62495eb
asharykin <aleksandr-sharykin@mail.ru>
2025-01-24 12:44:10 +0300

    Refactoring and minor changes in design

--- quiz/urls.py
+++ quiz/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('log-in', LoginView.as_view(), name='log_in'),
     path('log-out', LogoutView.as_view(), name='log_out'),
     path('access-test', AccessTestView.as_view(), name='access_test'),
-    path('test/<int:test_id>/description/', TestDescriptionView.as_view(), name='test_description'), 
-    path('test/<int:test_id>/', TestView.as_view(), name='take_test'),
+    path('test/<int:test_id>/description', TestDescriptionView.as_view(), name='test_description'),
+    path('test/<int:test_id>', TestView.as_view(), name='take_test'),
     path('results', UserTestResultsView.as_view(), name='user_results')
 ]
